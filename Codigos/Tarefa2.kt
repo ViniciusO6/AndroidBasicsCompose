7cefe3d41604caa4e2b721dc5c8b83f2e8374f94
Vinicius Augusto <125403644+ViniciusO6@users.noreply.github.com>
2024-06-27 13:54:26 -0300

    Add files via upload

---
+++ Codigos/Tarefa2.kt
@@ -0,0 +1,11 @@
+enum class Daypart {
+    MORNING,
+    AFTERNOON,
+    EVENING,
+}
+data class Event(
+    val title: String,
+    val description: String? = null,
+    val daypart: Daypart,
+    val durationInMinutes: Int,
+)
